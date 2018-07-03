c2cd3475ac331c1405ef36a9922342be3741bc62
LBWright <lwright.js@gmail.com>
2018-07-03 00:02:44 -0500

    Created CreatePost component

--- client/src/components/Posts/Rating.js
+++ client/src/components/Posts/Rating.js
@@ -11,46 +11,47 @@ const Star = Styled.span`
 `;
 
 class Rating extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state= {
-            stars: 3,
-            totalStars: 3,
-            totalRatings: 1,
-            ratingPercentage: 100
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      stars: 3,
+      totalStars: 3,
+      totalRatings: 1,
+      ratingPercentage: 100
+    };
+  }
 
-    addStars = (value) => {
-        this.setState({stars: value});
-        incrementTotal();
-        calculatePercentage();
-    }
+  addStars = value => {
+    this.setState({ stars: value });
+    this.incrementTotal();
+    this.calculatePercentage();
+  };
 
-    incrementTotal = () => {
-        this.setState({totalStars: this.state.totalStars + this.state.stars})
-    }
+  incrementTotal = () => {
+    this.setState({ totalStars: this.state.totalStars + this.state.stars });
+  };
 
-    incrementRatings = () => {
-        this.setState({totalRatings: this.state.totalRatings + 1})
-    }
+  incrementRatings = () => {
+    this.setState({ totalRatings: this.state.totalRatings + 1 });
+  };
 
-    calculatePercentage = () => {
-        const newTotal = (this.state.totalStars / (this.state.totalRatings * 5)) * 100;
-        this.setState({ratingPercentage: newTotal});
-    }
+  calculatePercentage = () => {
+    const newTotal =
+      (this.state.totalStars / (this.state.totalRatings * 5)) * 100;
+    this.setState({ ratingPercentage: newTotal });
+  };
 
-    render(){
-        return(
-            <Stars>
-            <FontAwesomeIcon icon='star' onClick={this.addStars(1)} /> 
-            <FontAwesomeIcon icon='star' onClick={this.addStars(2)} />
-            <FontAwesomeIcon icon='star' onClick={this.addStars(3)} />
-            <FontAwesomeIcon icon='star' onClick={this.addStars(4)} />
-            <FontAwesomeIcon icon='star' onClick={this.addStars(5)} />
-            </Stars>
-        )
-    }
+  render() {
+    return (
+      <Stars>
+        <FontAwesomeIcon icon="star" onClick={this.addStars(1)} />
+        <FontAwesomeIcon icon="star" onClick={this.addStars(2)} />
+        <FontAwesomeIcon icon="star" onClick={this.addStars(3)} />
+        <FontAwesomeIcon icon="star" onClick={this.addStars(4)} />
+        <FontAwesomeIcon icon="star" onClick={this.addStars(5)} />
+      </Stars>
+    );
+  }
 }
 
 export default Rating;
